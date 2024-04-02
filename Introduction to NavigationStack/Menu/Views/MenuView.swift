dab1d8b4fae9f9a93f6b15ee998c58ecae9e1edc
Danielle Lewis <103973410+dlewisdev@users.noreply.github.com>
2024-04-02 16:17:54 -0400

    - cart button now navigates to cart view using routerManager

--- Introduction to NavigationStack/Menu/Views/MenuView.swift
+++ Introduction to NavigationStack/Menu/Views/MenuView.swift
@@ -48,7 +48,7 @@ struct MenuView: View {
             .toolbar {
                 ToolbarItem(placement: .primaryAction) {
                     CartButton(count: cartManager.items.count) {
-                        
+                        routerManager.push(to: .cart)
                     }
                 }
             }
