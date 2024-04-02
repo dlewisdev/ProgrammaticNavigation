5b0df51d7fb649a0f4a5f5704c69015a2d77796f
Danielle Lewis <103973410+dlewisdev@users.noreply.github.com>
2024-04-02 18:45:37 -0400

    - add allergies case to Route enum

--- Introduction to NavigationStack/Route.swift
+++ Introduction to NavigationStack/Route.swift
@@ -12,6 +12,7 @@ enum Route {
     case menuItem(item: any MenuItem)
     case cart
     case ingredients(items: [Ingredient])
+    case allergies(items: [Allergie])
 }
 
 extension Route: Hashable {
@@ -30,6 +31,8 @@ extension Route: Hashable {
         case (.cart, .cart):
             return true
         case (.ingredients(let lhsItem), .ingredients(let rhsItem)):
+            return lhsItem == rhsItem
+        case (.allergies(let lhsItem), .allergies(let rhsItem)):
             return lhsItem == rhsItem
         default:
             return false
@@ -53,9 +56,10 @@ extension Route: View {
             }
         case .cart:
             CartView()
-            
         case .ingredients(let items):
             IngredientsDetailView(ingredients: items)
+        case .allergies(let items):
+            AllergiesDetailView(allergies: items)
         }
     }
 }
