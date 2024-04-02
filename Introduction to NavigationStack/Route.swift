d7a963dd79cc81c03c37b5d8972ad3f596974c9b
Danielle Lewis <103973410+dlewisdev@users.noreply.github.com>
2024-04-02 16:11:12 -0400

    - conform Route enum to View - add switch statement controlling nevigation into the enum view body

--- Introduction to NavigationStack/Route.swift
+++ Introduction to NavigationStack/Route.swift
@@ -6,8 +6,9 @@
 //
 
 import Foundation
+import SwiftUI
 
-enum Route: Hashable {
+enum Route: View, Hashable {
     case menuItem(item: any MenuItem)
     
     // Used to uniquley identify each case
@@ -21,6 +22,22 @@ enum Route: Hashable {
         switch (lhs, rhs) {
         case (.menuItem(let lhsItem), .menuItem(let rhsItem)):
             return lhsItem.id == rhsItem.id
+        }
+    }
+    
+    var body: some View {
+        switch self {
+        case .menuItem(let item):
+            switch item {
+            case is Food:
+                FoodDetailView(food: item as! Food)
+            case is Drink:
+                DrinkDetailView(drink: item as! Drink)
+            case is Dessert:
+                DessertDetailView(dessert: item as! Dessert)
+            default:
+                EmptyView()
+            }
         }
     }
 }
